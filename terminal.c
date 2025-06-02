7dfa65f81d6b0d8aee283c4e1d8ff0d697886277
Davi Batista <davideosmar11@gmail.com>
2025-06-02 09:35:04 -0300

    feat: added main method

--- terminal.c
+++ terminal.c
@@ -33,3 +33,29 @@ Flight* generateAddress(char origem[20], char destino[20], char numero[10], char
     newFlight->next = NULL;
     return newFlight;
 }
+
+int main()
+{
+    int opcao = -1;
+    char origem[20];
+    char destino[20];
+    char numero[10];
+    char data[11]; // Formato: YYYY-MM-DD
+    char hora[6];  // Formato: HH:MM
+    char companhia[20];
+    char tipo[20]; // "Econômico", "Primeira Classe"
+    char status[20]; // "Aguardando Embarque", "Atrasado", "Cancelado"
+    Flight* start = NULL;
+    Flight* end = NULL;
+
+    while( option != 0){
+        printf( "\n\n------Menu------\n");
+        printf( " 1 - Inserir novo voo na lista \n");
+        printf( " 2 - Remover voo da lista \n");
+        printf( " 3 - Mudar status do voo \n");
+        printf( " 4 - Mostrar o terminal \n");
+        printf( " 0 - Sair \n");
+        printf( " Digite sua escolha:  ");
+        scanf( "%d" , &option );
+    }
+}
